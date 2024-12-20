d89904af8545fbf3a173b78a2b7f81946c593c1e
Sirakusa <herrera.clip@gmail.com>
2024-12-19 18:12:34 -0600

    add/welcome-page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,24 @@
+import "react-native-gesture-handler";
 import "react-native-reanimated";
-import { Stack } from "expo-router/stack";
 import { Slot } from "expo-router";
 import { SessionProvider } from "@/hooks/sessionContext";
 import { AxiosProvider } from "@/hooks/axiosContext";
+import { createStackNavigator } from "@react-navigation/stack";
+import Index from ".";
+
+const Stack = createStackNavigator();
 
 export default function RootLayoutNav() {
   return (
     <SessionProvider>
       <AxiosProvider>
-        <Stack>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#714ABB" },
+            headerTitleStyle: { fontWeight: "semibold", color: "#FFFFFF" },
+          }}>
           <Slot />
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
+        </Stack.Navigator>
       </AxiosProvider>
     </SessionProvider>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,192 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import PagerView from "react-native-pager-view";
+import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
+import Animated from "react-native-reanimated";
 
 export default function Index() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageSelected = (e: any) => {
+    setCurrentPage(e.nativeEvent.position);
+  };
+
   return (
     <View style={styles.screen}>
-      <View style={[styles.container, styles.dropShadow]}>
-        <Text style={styles.title}>Welcome</Text>
-        <Link push href="/(auth)/logIn" style={styles.link}>
-          <Text style={styles.linkText}>Login</Text>
-        </Link>
-        <Link push href="/(auth)/signUp" style={styles.link}>
-          <Text style={styles.linkText}>Signup</Text>
-        </Link>
+      <StatusBar
+        style="light"
+        translucent={true}
+        backgroundColor="transparent"
+      />
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/welcome/page1/wallpaper.jpg")}
+          style={styles.image}
+        />
+      </View>
+
+      <Link
+        href={"/(auth)/logIn"}
+        style={{
+          position: "absolute",
+          right: 1,
+          padding: 30,
+          fontSize: 16,
+          fontWeight: "regular",
+          fontFamily: "Mulish-Black",
+          zIndex: 10,
+        }}>
+        Skip
+      </Link>
+
+      <PagerView
+        style={styles.ViewPageContainer}
+        initialPage={0}
+        onPageSelected={handlePageSelected}>
+        {/* Página 1 */}
+        <View style={{ flex: 1 }} key="1">
+          <View style={styles.imageFitnessContainer}>
+            <Image
+              style={styles.imageFitness}
+              source={require("../assets/images/welcome/page1/Fitness-stats.svg")}
+            />
+          </View>
+          <View style={styles.page}>
+            <View style={[styles.container, styles.dropShadow]}>
+              <View style={{ marginTop: 30, alignItems: "center" }}>
+                <Text style={styles.textTitle}>Welcome</Text>
+                <View style={{ flexDirection: "row" }}>
+                  <Text style={styles.textTitle}>to </Text>
+                  <Text style={[styles.textTitle, { color: "#512DA8" }]}>
+                    FitMe
+                  </Text>
+                </View>
+              </View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  marginTop: 15,
+                  fontSize: 16,
+                  width: "80%",
+                  color: "#404B52",
+                  fontWeight: "regular",
+                }}>
+                Discover FitMe on-demand workouts, designed to match your
+                available time and keep you moving
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        {/* Página 2 */}
+        <View style={{ flex: 1 }} key="2">
+          <View style={styles.imageFitnessContainer}>
+            <Image
+              style={styles.imageFitness}
+              source={require("../assets/images/welcome/page1/woman.svg")}
+            />
+          </View>
+          <View style={styles.page}>
+            <View style={[styles.container, styles.dropShadow]}>
+              <View style={{ marginTop: 30, alignItems: "center" }}>
+                <Text style={styles.textTitle}>Personalized Routines</Text>
+                <View style={{ flexDirection: "row" }}></View>
+              </View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  marginTop: 15,
+                  fontSize: 16,
+                  width: "80%",
+                  color: "#404B52",
+                  fontWeight: "regular",
+                }}>
+                Create your own fitness routines or explore shared routines from
+                your group. Whether you're looking to focus on strength, cardio,
+                or flexibility, the app adapts to your needs
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        {/* Página 3 */}
+        <View style={{ flex: 1 }} key="3">
+          <View style={styles.imageFitnessContainer}>
+            <Image
+              style={styles.imageFitness}
+              source={require("../assets/images/welcome/page1/woman-desk.svg")}
+            />
+          </View>
+          <View style={styles.page}>
+            <View style={[styles.container, styles.dropShadow]}>
+              <View style={{ marginTop: 30, alignItems: "center" }}>
+                <Text style={styles.textTitle}>Group Features</Text>
+                <View style={{ flexDirection: "row" }}></View>
+              </View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  marginTop: 15,
+                  fontSize: 16,
+                  width: "80%",
+                  color: "#404B52",
+                  fontWeight: "regular",
+                }}>
+                Exercise with friends, track your streaks, and share your
+                progress
+              </Text>
+            </View>
+          </View>
+        </View>
+      </PagerView>
+      <View style={styles.dotsContainer}>
+        {Array.from({ length: 3 }).map((_, index) => {
+          return (
+            <Animated.View
+              key={index}
+              style={[
+                styles.dot,
+                {
+                  height: 8,
+                  borderRadius: 8 / 2,
+                  marginRight: 8,
+                  backgroundColor:
+                    currentPage === index ? "#512DA8" : "#C9C9C9",
+                  width: currentPage === index ? 27 : 8,
+                },
+              ]}
+            />
+          );
+        })}
+      </View>
+      <View
+        style={{
+          justifyContent: "flex-end",
+          alignItems: "center",
+          paddingBottom: 40,
+        }}>
+        <LinearGradient
+          colors={["#7850BF", "#512DA8"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.button}>
+          <TouchableOpacity>
+            <Link push href={"/(auth)/logIn"}>
+              <Text style={styles.buttonText}>Get Started</Text>
+            </Link>
+          </TouchableOpacity>
+        </LinearGradient>
+        <Text style={{ fontSize: 14, color: "#404B52", marginTop: 20 }}>
+          Already have account?{" "}
+          <Link push href="/(auth)/signUp">
+            <Text style={{ fontSize: 16, color: "#461B93", fontWeight: 500 }}>
+              Sign up
+            </Text>
+          </Link>
+        </Text>
       </View>
     </View>
   );
@@ -21,27 +195,18 @@ export default function Index() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "flex-end",
     backgroundColor: "#ffffff",
+  },
+  page: {
+    flex: 1,
+    justifyContent: "flex-end",
   },
   container: {
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    alignItems: "center",
-    height: "47%",
+    height: "34%",
     backgroundColor: "#ffffff",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  link: {
-    marginVertical: 10,
-  },
-  linkText: {
-    fontSize: 18,
-    color: "blue",
+    alignItems: "center",
   },
   dropShadow: {
     shadowColor: "#000000",
@@ -52,5 +217,57 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.12,
     shadowRadius: 36,
     elevation: 30,
+  },
+  ViewPageContainer: {
+    flex: 1,
+  },
+  imageContainer: {
+    position: "absolute",
+    top: -60,
+    left: 0,
+    width: "100%",
+    height: 400,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageFitnessContainer: {
+    position: "absolute",
+    top: 45,
+    left: 16,
+    width: "100%",
+    height: 400,
+  },
+  imageFitness: {
+    width: "90%",
+    height: "90%",
+  },
+  textTitle: {
+    fontSize: 30,
+    fontWeight: "semibold",
+    fontFamily: "Montserrat-SemiBold",
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 26,
+    width: "60%",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 14,
+    textAlign: "center",
+    fontWeight: "semibold",
+    fontFamily: "Montserrat-SemiBold",
+  },
+  dotsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  dot: {
+    backgroundColor: "#C9C9C9",
   },
 });
